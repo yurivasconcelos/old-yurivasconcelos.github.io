1e2a0772ec54e20f5bc9540a684bcc17f589d333
Yuri <yurifx@gmail.com>
2024-07-06 19:02:36 +1200

    adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import './App.css';
 // import { Button } from './components/Button';
 // import { Effect } from './components/Effect';
 // import { FetchComponent } from './components/Fetch';
-import { SlackComponent } from './components/SlackComponent';
-import { TestComponent } from './components/TestsComponent';
+// import { SlackComponent } from './components/SlackComponent';
+// import { TestComponent } from './components/TestsComponent';
 
 function App() {
   return (
     <>
       <div>
-        {/* <Button></Button>
-        <Effect name={count.toString()}></Effect>
-        <FetchComponent></FetchComponent> */}
-        <SlackComponent />
-        <TestComponent />
-
+        <p>Loaded!</p>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/test`}>Test</a>
+            </li>
+            <li>
+              <a href={`/slack`}>Slack</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import { TestComponent } from './components/TestsComponent.tsx';
+import { SlackComponent } from './components/SlackComponent.tsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App/> },
+  { path: '/test', element: <TestComponent/> },
+  { path: '/slack', element: <SlackComponent/> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

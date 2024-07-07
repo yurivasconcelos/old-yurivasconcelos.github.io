2aa6a901221e9ab1ff5fda4178f41d9412550337
Yuri <yurifx@gmail.com>
2024-07-07 19:04:25 +1200

    trying hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import { TestComponent } from './components/TestsComponent.tsx';
 import { SlackComponent } from './components/SlackComponent.tsx';
 import SignInComponent from './components/LoginComponent.tsx';
@@ -42,8 +42,35 @@ const router = createBrowserRouter([
   },
 ]);
 
+console.log(router);
+
+
+export const hashRouter = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <LoginComponent /> },
+      { path: '/home', element: <HomeComponent /> },
+      { path: '/new', element: <NewComponent /> },
+      { path: '/test', element: <TestComponent /> },
+      { path: '/test/:id', element: <TestComponent /> },
+      { path: '/slack', element: <SlackComponent /> },
+      { path: '/signin', element: <SignInComponent /> },
+      { path: '/forms', element: <FormsComponent /> },
+      { path: '/marketing', element: <MarketingCard /> },
+      { path: '/flex', element: <FlexboxContainer /> },
+      { path: '/flexitem', element: <FlexboxContainerItem /> },
+      { path: '/calendar', element: <Calendar /> },
+      { path: '/tab', element: <TabComponent /> },
+      { path: '/grid', element: <GridComponent /> },
+      { path: '/formgrid', element: <FormGridComponent /> },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={hashRouter} />
   </React.StrictMode>
 );

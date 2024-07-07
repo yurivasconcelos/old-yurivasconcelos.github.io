08e25bd5981edda31458f28ddfdfa6ce9a8de85c
Yuri <yurifx@gmail.com>
2024-07-07 19:25:51 +1200

    fixing sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,20 +2,21 @@ import { Link } from 'react-router-dom';
 
 export function Sidebar() {
   return (
-    <div className='invisible w-0 lg:w-72 lg:visible bg-slate-200'>
-      <div className='pl-4 pt-2 w-40'>
+    <div className='invisible w-0 lg:w-72 pl-8 lg:visible bg-slate-200'>
+      
+      <div className='pt-8 w-40'>
         <Link to={`formgrid`}>FormsGrid</Link>
       </div>
 
-      <div className='pl-4 pt-2 w-40'>
+      <div className='pt-4 w-40'>
         <Link to={`test`}>test</Link>
       </div>
 
-      <div className='pl-4 pt-2 w-40'>
+      <div className='pt-4'>
         <Link to={`new`}>new component</Link>
       </div>
       
-      <div className='pl-4 pt-2 w-40'>
+      <div className='pt-4'>
         <Link to={`tab`}>tabs</Link>
       </div>
     </div>

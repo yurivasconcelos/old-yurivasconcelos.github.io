dc790634113ece91268156abdc1a6cdbb2efce8d
Yuri <yurifx@gmail.com>
2024-07-07 14:51:00 +1200

    fix form on dark mobile

--- src/components/AlertComponent.tsx
+++ src/components/AlertComponent.tsx
@@ -3,7 +3,7 @@ export function AlertComponent() {
     <>
       {/* alert example - https://flowbite.com/docs/components/alerts/ */}
       <div
-        className='p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400'
+        className='p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50'
         role='alert'>
         <span className='font-medium'>Info alert!</span> Change a few things up and try submitting again.
       </div>
--- src/components/FormsComponent.tsx
+++ src/components/FormsComponent.tsx
@@ -26,25 +26,25 @@ export function FormsComponent() {
       <div className=''>
         <form className='max-w-sm mx-auto'>
           <div className='mb-5'>
-            <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+            <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900  '>
               Your email
             </label>
             <input
               type='email'
               id='email'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
               placeholder='name@flowbite.com'
               required
             />
           </div>
           <div className='mb-5'>
-            <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+            <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900  '>
               Your password
             </label>
             <input
               type='password'
               id='password'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
               required
             />
           </div>
@@ -54,7 +54,7 @@ export function FormsComponent() {
                 id='remember'
                 type='checkbox'
                 value=''
-                className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
+                className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300       dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
                 required
               />
             </div>
@@ -94,113 +94,111 @@ export function FormsComponent() {
           <form>
             <div className='grid gap-6 mb-6 md:grid-cols-2'>
               <div>
-                <label htmlFor='first_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='first_name' className='block mb-2 text-sm font-medium text-gray-900  '>
                   First name
                 </label>
                 <input
                   type='text'
                   id='first_name'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder='John'
                   required
                 />
               </div>
               <div>
-                <label htmlFor='last_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='last_name' className='block mb-2 text-sm font-medium text-gray-900  '>
                   Last name
                 </label>
                 <input
                   type='text'
                   id='last_name'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder='Doe'
                   required
                 />
               </div>
               <div>
-                <label htmlFor='company' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='company' className='block mb-2 text-sm font-medium text-gray-900  '>
                   Company
                 </label>
                 <input
                   type='text'
                   id='company'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder='Flowbite'
                   required
                 />
               </div>
               <div>
-                <label htmlFor='phone' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='phone' className='block mb-2 text-sm font-medium text-gray-900  '>
                   Phone number
                 </label>
                 <input
                   type='tel'
                   id='phone'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder='123-45-678'
                   pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
                   required
                 />
               </div>
               <div>
-                <label htmlFor='website' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='website' className='block mb-2 text-sm font-medium text-gray-900  '>
                   Website URL
                 </label>
                 <input
                   type='url'
                   id='website'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder='flowbite.com'
                   required
                 />
               </div>
               <div>
-                <label htmlFor='visitors' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+                <label htmlFor='visitors' className='block mb-2 text-sm font-medium text-gray-900  '>
                   Unique visitors (per month)
                 </label>
                 <input
                   type='number'
                   id='visitors'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                   placeholder=''
                   required
                 />
               </div>
             </div>
             <div className='mb-6'>
-              <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+              <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900  '>
                 Email address
               </label>
               <input
                 type='email'
                 id='email'
-                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                 placeholder='john.doe@company.com'
                 required
               />
             </div>
             <div className='mb-6'>
-              <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+              <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900  '>
                 Password
               </label>
               <input
                 type='password'
                 id='password'
-                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                 placeholder='•••••••••'
                 required
               />
             </div>
             <div className='mb-6'>
-              <label
-                htmlFor='confirm_password'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+              <label htmlFor='confirm_password' className='block mb-2 text-sm font-medium text-gray-900  '>
                 Confirm password
               </label>
               <input
                 type='password'
                 id='confirm_password'
-                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5           '
                 placeholder='•••••••••'
                 required
               />
@@ -211,7 +209,7 @@ export function FormsComponent() {
                   id='remember'
                   type='checkbox'
                   value=''
-                  className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800'
+                  className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300       dark:focus:ring-blue-600 dark:ring-offset-gray-800'
                   required
                 />
               </div>

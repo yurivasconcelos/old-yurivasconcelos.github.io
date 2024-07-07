14c860d92059a83e3df9893cfc2988b83333dc15
Yuri <yurifx@gmail.com>
2024-07-07 19:16:55 +1200

    adding sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export function Sidebar() {
   return (
-    <div  className='invisible w-0 lg:w-72 lg:visible'>
-      <div>Menu</div>
-      <div>Menu</div>
-      <div>Menu</div>
-      <div>Menu</div>
-      <div>Menu</div>
+    <div className='invisible w-0 lg:w-72 lg:visible bg-slate-200'>
+      <div className='pl-4 pt-2 w-40'>
+        <Link to={`formgrid`}>FormsGrid</Link>
+      </div>
+      <div className='pl-4 pt-2 w-40'>
+        <Link to={`test`}>test</Link>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { TestComponent } from './components/TestsComponent.tsx';
 import { SlackComponent } from './components/SlackComponent.tsx';
 import SignInComponent from './components/LoginComponent.tsx';
@@ -18,33 +18,7 @@ import { GridComponent } from './components/GridComponent.tsx';
 import HomeComponent from './components/HomeComponent.tsx';
 import { FormGridComponent } from './components/FormGridComponent.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: '/', element: <LoginComponent /> },
-      { path: '/home', element: <HomeComponent /> },
-      { path: '/new', element: <NewComponent /> },
-      { path: '/test', element: <TestComponent /> },
-      { path: '/test/:id', element: <TestComponent /> },
-      { path: '/slack', element: <SlackComponent /> },
-      { path: '/signin', element: <SignInComponent /> },
-      { path: '/forms', element: <FormsComponent /> },
-      { path: '/marketing', element: <MarketingCard /> },
-      { path: '/flex', element: <FlexboxContainer /> },
-      { path: '/flexitem', element: <FlexboxContainerItem /> },
-      { path: '/calendar', element: <Calendar /> },
-      { path: '/tab', element: <TabComponent /> },
-      { path: '/grid', element: <GridComponent /> },
-      { path: '/formgrid', element: <FormGridComponent /> },
-    ],
-  },
-]);
-
-console.log(router);
-
-
+//why hashrouter https://stackoverflow.com/questions/71984401/react-router-not-working-with-github-pages
 export const hashRouter = createHashRouter([
   {
     path: '/',

a2ff6098fe07398bb99657ab057310b7b0ec6a96
Yuri <yurifx@gmail.com>
2024-07-07 23:36:31 +1200

    fix for bigger screens

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <div className="flex flex-wrap items-center justify-between mx-auto p-4">
     <a
       href="https://flowbite.com/"
       className="flex items-center space-x-3 rtl:space-x-reverse"
@@ -67,14 +67,14 @@ const Navbar = () => {
         </li>
         <li>
           <Link
-            to="customers"
+            to="forms"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Customers
           </Link>
         </li>
         <li>
-          <Link to="services"
+          <Link to="marketing"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           >
             Services

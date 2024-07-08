292aafb5868c46e2ece22079f5b796207d04eb39
Yuri <yurifx@gmail.com>
2024-07-08 23:07:48 +1200

    adding recharts + lucide

--- src/components/TestsComponent.tsx
+++ src/components/TestsComponent.tsx
@@ -1,12 +1,69 @@
 import { useLocation } from 'react-router-dom';
 import { Button } from './@shadcn/button';
+import { Monitor } from 'lucide-react';
+
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from './@shadcn/chart';
+
 <script src='https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js'></script>;
 
 export function TestComponent() {
+  const chartData = [
+    { month: 'January', desktop: 186, mobile: 80 },
+    { month: 'February', desktop: 305, mobile: 200 },
+    { month: 'March', desktop: 237, mobile: 120 },
+    { month: 'April', desktop: 73, mobile: 190 },
+    { month: 'May', desktop: 209, mobile: 130 },
+    { month: 'June', desktop: 214, mobile: 140 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: 'Desktop',
+      color: '#2563eb',
+    },
+    mobile: {
+      label: 'Mobile',
+      color: '#60a5fa',
+    },
+  } satisfies ChartConfig;
+
   const location = useLocation();
   console.log(location);
   return (
     <>
+      <ChartContainer config={chartConfig} className='min-h-[200px] w-1/2'>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey='month'
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+          <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+        </BarChart>
+      </ChartContainer>
+
+      <ChartContainer config={chartConfig} className='h-[200px] w-full'>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey='month'
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+          <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+        </BarChart>
+      </ChartContainer>
+      <Monitor size={120}></Monitor>
       <div className='flex flex-col items-center justify-center w-300'>
         <div>Hey</div>
         <Button className='w-full lg:w-1/2 flex items-center justify-center'></Button>

9eee8c7c7e81a5802638b93ce78681cfdca440ac
Yuri <yurifx@gmail.com>
2024-07-09 00:33:42 +1200

    fix build

--- src/components/@shadcn/alert-dialog.tsx
+++ src/components/@shadcn/alert-dialog.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from "utils"
-import { buttonVariants } from "src/components/@shadcn//button"
+import { cn } from "./utils";
+import { buttonVariants } from "./button";
 
 const AlertDialog = AlertDialogPrimitive.Root
 

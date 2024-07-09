cba0bb03aba9f8584a2e81ad435833fdfe013161
Yuri <yurifx@gmail.com>
2024-07-09 22:46:09 +1200

    adding a bunch of other things

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,10 +1,9 @@
-import { Card } from '../@shadcn/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../@shadcn/card';
 import { Button } from '../@shadcn/button';
 import { toast } from 'sonner';
-
-import { Popover, PopoverContent, PopoverTrigger } from '../@shadcn/popover';
 import { Label } from '../@shadcn/label';
 import { Input } from '../@shadcn/input';
+import { Popover, PopoverContent, PopoverTrigger } from '../@shadcn/popover';
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '../@shadcn/resizable';
 import {
   Select,
@@ -37,6 +36,12 @@ import {
 } from '../@shadcn/navigation-menu';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../@shadcn/tooltip';
 import { Toggle } from '../@shadcn/toggle';
+import { Textarea } from '../@shadcn/textarea';
+import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from '../@shadcn/table';
+import { IconJarLogoIcon } from '@radix-ui/react-icons';
+import { Skeleton } from '../@shadcn/skeleton';
+import { ScrollArea } from '../@shadcn/scroll-area';
+import { Menubar, MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem, MenubarShortcut, MenubarSeparator } from '../@shadcn/menubar';
 
 export function TestComponent() {
   return (
@@ -124,10 +129,107 @@ export function TestComponent() {
         </Select>
       </div>
 
-      <div>
+      <div className='p-10 my-10 w-1/2 bg-grey-50'>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[100px]'>Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className='text-right'>Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className='font-medium'>INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className='text-right'>$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableBody>
+            <TableRow>
+              <TableCell className='font-medium'>INV001</TableCell>
+              <TableCell>
+                <IconJarLogoIcon />
+              </TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className='text-right'>$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className='p-10 my-10 w-1/2 bg-purple-50'>
+        <div className='flex items-center space-x-4'>
+          <Skeleton className='h-12 w-12 rounded-full' />
+          <div className='space-y-2'>
+            <Skeleton className='h-4 w-[250px]' />
+            <Skeleton className='h-4 w-[200px]' />
+          </div>
+        </div>{' '}
+      </div>
+
+      <div className='p-10 my-10 w-1/2 bg-cyan-50'>
+        <ScrollArea className='h-72 w-48 rounded-md border'></ScrollArea>
+      </div>
+
+      <div className='p-10 my-10 w-full bg-purple-50'>
+        <Tabs defaultValue='account' className='w-[400px]'>
+          <TabsList className='grid w-full grid-cols-2'>
+            <TabsTrigger value='account'>Account</TabsTrigger>
+            <TabsTrigger value='password'>Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value='account'>
+            <Card>
+              <CardHeader>
+                <CardTitle>Account</CardTitle>
+                <CardDescription>Make changes to your account here. Click save when you're done.</CardDescription>
+              </CardHeader>
+              <CardContent className='space-y-2'>
+                <div className='space-y-1'>
+                  <Label htmlFor='name'>Name</Label>
+                  <Input id='name' defaultValue='Pedro Duarte' />
+                </div>
+                <div className='space-y-1'>
+                  <Label htmlFor='username'>Username</Label>
+                  <Input id='username' defaultValue='@peduarte' />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save changes</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value='password'>
+            <Card>
+              <CardHeader>
+                <CardTitle>Password</CardTitle>
+                <CardDescription>Change your password here. After saving, you'll be logged out.</CardDescription>
+              </CardHeader>
+              <CardContent className='space-y-2'>
+                <div className='space-y-1'>
+                  <Label htmlFor='current'>Current password</Label>
+                  <Input id='current' type='password' />
+                </div>
+                <div className='space-y-1'>
+                  <Label htmlFor='new'>New password</Label>
+                  <Input id='new' type='password' />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button>Save password</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+
+      <div className='p-10 my-10 w-full bg-green-50'>
         <Sheet>
           <SheetTrigger>
-            <Button variant='outline'>Open</Button>
+            <Button variant='outline'>Open right panel medium</Button>
           </SheetTrigger>
           <SheetContent side='right' className='w-[380px] lg:min-w-[600px]'>
             <SheetHeader>
@@ -156,6 +258,23 @@ export function TestComponent() {
           </SheetContent>
         </Sheet>
       </div>
+
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+
       <Button
         variant='outline'
         onClick={() =>
@@ -174,7 +293,8 @@ export function TestComponent() {
         <Switch onClick={() => toast('Hey Yuri')} />
         <Toggle className=''>toggle</Toggle>
         <Label htmlFor='email'>Just a label</Label>
-        <Input className='mt-8'/>
+        <Input className='mt-8' />
+        <Textarea />
       </div>
 
       <div className='p-10 my-10 w-full bg-gray-50'>
